220e3db3a26bf1b1e87c8ed299b349a2033f62fa
arol1122 <arthur.oliviera@netcracker.com>
2025-08-06 14:02:27 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+},
+{
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+},
+];
+
+function Menu(){
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+            <ul className="pizzas">
+                { pizzaData.map((pizza) => {
+                    return <Pizza pizzaData={ pizza } />
+                }) }
+            </ul>
+        </main>
+    );
+}
+
+function App() {
+    return (
+        <div className="footer">
+        <Header />
+        <Menu />
+        <Footer />
+   </div>
+
+  );
+}
+
+function Header() {
+    return (
+        <header className="header">
+            <div>
+                <h1>Pizza Planet</h1>
+            </div>
+        </header>
+    );
+}
+
+
+function Footer(){
+    return (
+        <div>
+            <footer className="footer"> All rights reserved 2025</footer>
+        </div>
+    );
+}
+
+function Pizza(props){
+    return (
+        <li className="pizza">
+            <img src={props.pizzaData.photoName} alt={props.pizzaData.name}/>
+            <div>
+                <h3>{props.pizzaData.name}</h3>
+                <p>{props.pizzaData.ingredients}</p>
+                <span>{props.pizzaData.price}</span>
+            </div>
+        </li>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
